9d281676329605f84768ec0edfaf92b08ca4f316
justheimsk <henrique.mdimer@gmail.com>
2024-07-08 22:08:28 +0000

    feat: add avatar ui component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,11 @@
+import './Avatar.style.scss';
+
+export function Avatar() {
+  return (
+    <>
+      <div className="avatar--container">
+        <div className="avatar" />
+      </div>
+    </>
+  );
+}
