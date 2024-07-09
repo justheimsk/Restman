884a7dfdb7b58f76dc1deb89f51a3bc0f4452d87
justheimsk <henrique.mdimer@gmail.com>
2024-07-09 20:17:51 +0000

    chore: display Layout on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from '@components/Navbar/Navbar';
 import '../globals.scss';
+import { Layout } from '@components/Layout';
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Layout />
     </>
   );
 };
