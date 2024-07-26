101d5813516103ff373422b89f8777a2be7ae354
justheimsk <henrique.mdimer@gmail.com>
2024-07-26 20:57:22 -0300

    chore add React hooks for store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import httpResponseReducer from './httpResponse';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     httpResponse: httpResponseReducer,
   },
 });
+
+export default store;
+
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
